92b768bfc2421ee2700bcdbd7239611c38878426
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-07 06:19:35 +0800

    Sun Sep  7 06:19:35 AM CST 2025

--- .make/depend.dir/libplatform.mk
+++ .make/depend.dir/libplatform.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libplatform.recursive
 ECHO_TARGETS += libplatform.echo
 libplatform.recursive:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libsystem/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libsystem/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/ && make recursive && make install
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libsystem/               >> /home/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libsystem/.make >> /home/lidali/alpine/libraries/libplatform/.make/super
+	cd /home/lidali/alpine/libraries/libplatform/ && make recursive && make install
+	-rm /home/lidali/alpine/libraries/libplatform/.make/super
 libplatform.echo:
-	@echo SUPER_MAKE_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libsystem/               >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libsystem/.make >> /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
-	cd /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/ && make echo
-	-rm /mnt/d/CORE_WEB_SYSTEM_WEB/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libsystem/               >> /home/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libsystem/.make >> /home/lidali/alpine/libraries/libplatform/.make/super
+	cd /home/lidali/alpine/libraries/libplatform/ && make echo
+	-rm /home/lidali/alpine/libraries/libplatform/.make/super
