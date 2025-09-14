765d0b9169f24ad2d27d037f29f02300c84123c0
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:31 +0800

    Mon Sep 15 07:35:31 AM CST 2025

--- .make/depend.dir/libplatform.mk
+++ .make/depend.dir/libplatform.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libplatform.recursive
 ECHO_TARGETS += libplatform.echo
 libplatform.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libsystem/               >> /home/lidali/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libsystem/.make >> /home/lidali/alpine/libraries/libplatform/.make/super
-	cd /home/lidali/alpine/libraries/libplatform/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	cd /volumes/llama/home/alpine/libraries/libplatform/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
 libplatform.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libsystem/               >> /home/lidali/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libsystem/.make >> /home/lidali/alpine/libraries/libplatform/.make/super
-	cd /home/lidali/alpine/libraries/libplatform/ && make echo
-	-rm /home/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	cd /volumes/llama/home/alpine/libraries/libplatform/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
