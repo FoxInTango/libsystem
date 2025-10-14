1bbda16ed14c5b6f1be07d44e1e9792c14187673
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:49 +0800

    2025年10月14日 星期二 15时57分49秒 CST

--- .make/depend.dir/libkernel.mk
+++ .make/depend.dir/libkernel.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libkernel.recursive
 ECHO_TARGETS += libkernel.echo
 libkernel.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libkernel/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libkernel/.make/super
-	cd /volumes/llama/home/alpine/libraries/libkernel/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libkernel/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libsystem/               >> /Users/lidali/alpine/libraries/libkernel/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libsystem/.make >> /Users/lidali/alpine/libraries/libkernel/.make/super
+	cd /Users/lidali/alpine/libraries/libkernel/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libkernel/.make/super
 libkernel.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libkernel/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libkernel/.make/super
-	cd /volumes/llama/home/alpine/libraries/libkernel/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libkernel/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libsystem/               >> /Users/lidali/alpine/libraries/libkernel/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libsystem/.make >> /Users/lidali/alpine/libraries/libkernel/.make/super
+	cd /Users/lidali/alpine/libraries/libkernel/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libkernel/.make/super
--- .make/depend.dir/libplatform.mk
+++ .make/depend.dir/libplatform.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libplatform.recursive
 ECHO_TARGETS += libplatform.echo
 libplatform.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	cd /volumes/llama/home/alpine/libraries/libplatform/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libsystem/               >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libsystem/.make >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	cd /Users/lidali/alpine/libraries/libplatform/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libplatform/.make/super
 libplatform.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libsystem/               >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libsystem/.make >> /volumes/llama/home/alpine/libraries/libplatform/.make/super
-	cd /volumes/llama/home/alpine/libraries/libplatform/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libsystem/               >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libsystem/.make >> /Users/lidali/alpine/libraries/libplatform/.make/super
+	cd /Users/lidali/alpine/libraries/libplatform/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libplatform/.make/super
